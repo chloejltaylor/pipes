50e7224cfab62b813316315b0dc9d8a0bfb869c1
Chloe Taylor <chloejltaylor@gmail.com>
2023-01-09 09:55:42 +0000

    add three working levels and an animation mp4

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+//appear and disappear logic
+
+function appear(el){
+  el.classList.remove("displaynone")
+}
+
+function disappear(el){
+  el.classList.add("displaynone")
+}
+
+
 //scaling to different devices
 
 
@@ -13,36 +24,134 @@ scale = Math.min(
   h / elHeight
 );
 
-console.log(scale)
 
 el.style.transform = "translate(-50%, -50%) " + "scale(" + scale + ")"
 
 //start game
 
 let titleplaybutton = document.getElementById("titleplaybutton")
-let header = document.getElementById("header")
-let titlescreen = document.querySelector(".titlescreen")
-let maingame = document.querySelector("main")
+    header = document.getElementById("header")
+    titlescreen = document.querySelector(".titlescreen")
+    welldonescreen = document.querySelector(".welldonescreen")
+    maingame = document.querySelector("main")
+    openinstructionsbutton = document.getElementById("openinstructionsbutton")
+    closeinstructionsbutton = document.getElementById("closeinstructionsbutton")
+    startagainbutton = document.getElementById("startagainbutton")
+    instructionsimage = document.querySelector(".instructionsimage")
+    gamegrid1 = document.querySelectorAll(".gamegrid1")
+    gamegrid2 = document.querySelectorAll(".gamegrid2")
+    gamegrid3 = document.querySelectorAll(".gamegrid3")
+    game1list = ["1a","1b"]
+    game2list = ["2a"]
+    game3list = ["3a"]
+    game1Selector = Math.floor((Math.random()) * 2)
+    game2Selector = Math.floor((Math.random()) * 1)
+    game3Selector = Math.floor((Math.random()) * 1)
+    game1 = game1list[game1Selector]
+    game2 = game2list[game2Selector]
+    game3 = game3list[game3Selector]
+    game1div = gamegrid1[game1Selector]
+    game2div = gamegrid2[game2Selector]
+    game3div = gamegrid3[game3Selector]
+    video = document.getElementById("videos")
+    start = document.querySelector(".start")
+    end = document.querySelector(".end")
+
+
+
+
 function startgame () {
-  console.log("startgame")
   setTimeout(() => {
-    header.classList.remove("displaynone")
-    maingame.classList.remove("displaynone")
-    titlescreen.classList.add("displaynone")
+    appear(header)
+    appear(maingame)
+    disappear(titlescreen)
+    appear(instructionsimage)
+    appear(game1div)
   }, 500);
 }
+function restartgame () {
+  console.log("restart")
+    // appear(game1div)
+    // disappear(game2div)
+    // disappear(game3div)
+    // disappear(welldonescreen)
+    // disappear(video)
+    // if(game1Selector==1) {
+    //   game1Selector = 0
+    //   game1 = game1list[game1Selector]
+    // }
+    // else {game1Selector++}
+    // if(game2Selector==3) {game2Selector = 0}
+    // else {game2Selector++}
+    // if(game3Selector==3) {game3Selector = 0}
+    // else {game3Selector++}
+    
+    setuptiles(tilesl1, positions_l1)
+    // setuptiles(tilesl2, positions_l2)
+    // setuptiles(tilesl3, positions_l3)
+}
 
-titleplaybutton.addEventListener("mousedown",startgame)
+titleplaybutton.addEventListener("pointerdown",startgame)
+startagainbutton.addEventListener("pointerdown",restartgame)
+
+  // start level 2
+
+  function startlevel2(){
+    for(let i=0; i<gamegrid1.length; i++){
+      disappear(gamegrid1[i])
+    }
+    for(let i=0; i<gamegrid2.length; i++){
+      appear(gamegrid2[i])
+    } 
+    disappear(continuel2)
+    disappear(video)
+    start.classList.remove("start_pos1_1")
+    start.classList.add("start_pos2_1")
+    end.classList.remove("end_pos1_1")
+    end.classList.add("end_pos2_1")
+    gameobjectsidle()
+  }
+
+    // start level 3
+
+  function startlevel3(){
+    for(let i=0; i<gamegrid2.length; i++){
+      disappear(gamegrid2[i])
+    }
+    for(let i=0; i<gamegrid3.length; i++){
+      appear(gamegrid3[i])
+    } 
+    disappear(continuel3)
+    disappear(video)
+    start.classList.remove("start_pos2_1")
+    start.classList.add("start_pos3_1")
+    end.classList.remove("end_pos2_1")
+    end.classList.add("end_pos3_1")
+    gameobjectsidle()
+  }
+
+// instructions
+
+closeinstructionsbutton.addEventListener("pointerdown", closeinstructions)
+openinstructionsbutton.addEventListener("pointerdown", openinstructions)
+
+function closeinstructions() {
+  disappear(instructionsimage)
+}
+
+function openinstructions() {
+  appear(instructionsimage)
+}
+
 
 //sounds
 const incorrect = new Audio("assets/sounds/incorrect.wav")
 const correct = new Audio("assets/sounds/correct.mp3")
 const win = new Audio("assets/sounds/win.wav")
-
+const swish = new Audio("assets/sounds/swish.wav")
 
 function playSound(sound){
 
-    console.log(sound)
     if(ismuted==false){
         sound.play()
     }
@@ -50,15 +159,18 @@ function playSound(sound){
 
 // get an array of tiles for level 1- tilesl1
 
-
 let tilesl1 = document.querySelectorAll(".tile_l1")
+let tilesl2 = document.querySelectorAll(".tile_l2")
+let tilesl3 = document.querySelectorAll(".tile_l3")
+
 
 //define an array of posible rotations
 let rotationarray=[0,90,180,270]
 
 //initialise the rotation positions
 let positions_l1 = [0,0,0,0]
-
+let positions_l2 = [0,0,0,0,0,0,0,0,0]
+let positions_l3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 //set up the tiles to start the game 
 
@@ -73,76 +185,142 @@ function rotate(tile) {
   return position
 }
 
-function setuptiles(){
-  for(let i=0; i<tilesl1.length; i++){
-    rotate(tilesl1[i])
+function setuptiles(tilesarray, positionsarray){
+  for(let i=0; i<tilesarray.length; i++){
+    rotate(tilesarray[i])
 
     //keep a record of the position of each tile initially
-    positions_l1[i] = position
-  }
-}
+    positionsarray[i] = position
 
-setuptiles()
-
-
-//when a tile is clicked, move it 90deg
-//"i" means the tile being clicked, left to right, top to bottom
-for(let i=0; i<tilesl1.length; i++){
-  tilesl1[i].addEventListener("mousedown", rotateonclick)
-
-  function rotateonclick(){
-    tilesl1[i].style.pointerEvents="none"
-    tilesl1[i].classList.remove("rotate90")
-
-    //set the rotation to 90deg of current position
-    //update the position array to reflect new position
-
-      rotation= rotationarray[positions_l1[i]]+90
-      positions_l1[i]++
-      tilesl1[i].classList.add("rotate90")
-    
-    console.log(positions_l1[i])
-    tilesl1[i].style.transform = 'rotate('+rotation+'deg)';
-    
-
-    setTimeout(() => {
-      tilesl1[i].style.pointerEvents="auto"
-      
-      if(positions_l1[i]==4){
-        positions_l1[i] = 0
-        tilesl1[i].classList.remove("rotate90")
-        tilesl1[i].style.transform = 'rotate('+0+'deg)';
-      }
-      testgamecomplete()
-    }, 1000);
-
-    
+  //make sure the tiles are not already correctly lined up
 
   }
 
+  function checkalignment(){
+    if(game1 == "1a" && positionsarray[0]==2 && (positionsarray[1]==3 && positionsarray[2]==0) && positionsarray[3]==1){
+      console.log("oh no, the game is already finished before it's begun")
+      setuptiles(tilesl1, positions_l1)
+    } else
+    if(game1 == "1b" && (positionsarray[6]==0 || positionsarray[6]==2) && (positionsarray[7]==0 || positionsarray[7]==2)){
+      console.log("oh no, the game is already finished before it's begun")
+      setuptiles(tilesl1, positions_l1)
+    } else {
+      console.log("good to go")
+    }
+  }
+
+  checkalignment()
+
+
 }
+
+setuptiles(tilesl1, positions_l1)
+setuptiles(tilesl2, positions_l2)
+setuptiles(tilesl3, positions_l3)
 
 //set up solution
 
 let continuel2 = document.getElementById("continuel2")
+let continuel3 = document.getElementById("continuel3")
 let turbine = document.querySelector(".turbine")
 let lightbulb = document.querySelector(".lightbulb")
 
-function testgamecomplete(){
-  if(positions_l1[1]==2 && (positions_l1[2]==0 || positions_l1[2]==2) && positions_l1[3]==0){
-    setTimeout(() => {
-      continuel2.classList.remove("displaynone")
-
-    }, 1000);
-    console.log("you win!")
-    turbine.classList.remove("turbinebefore")
-    turbine.classList.add("turbineglow")
-    lightbulb.classList.remove("lightbulbbefore")
-    lightbulb.classList.add("lightbulbglow")
-
-    playSound(win)
-  } else {
-    console.log("keep going")
+function testgamecompletel1(){
+  if(game1 == "1a" && positions_l1[0]==2 && positions_l1[1]==3 && positions_l1[2]==0 && positions_l1[3]==1){
+    gamewin("level1a")
+  }
+  if(game1 == "1b" && (positions_l1[6]==0 || positions_l1[6]==2) && (positions_l1[7]==0 || positions_l1[7]==2)){
+    gamewin("level1b")
   }
 }
+
+function testgamecompletel2(){
+  if(game2 == "2a" 
+     && positions_l2[0]==2 
+     && (positions_l2[1]==0 || positions_l2[1]==2)
+     && positions_l2[2]== 3
+     && positions_l2[3]== 1
+     && positions_l2[4]== 3
+     && (positions_l2[5]==1 || positions_l2[5]==3)
+     && (positions_l2[6]==0 || positions_l2[6]==2)
+     && positions_l2[7]== 0
+     && positions_l2[8]== 1
+  ){
+    gamewin("level2a")
+  }
+}
+
+function testgamecompletel3(){
+  if(game3 == "3a"
+      && positions_l3[0]==2 
+      && (positions_l3[1]==0 || positions_l3[1]==2)
+      && positions_l3[2]==3
+      && positions_l3[4]==0
+      && (positions_l3[6]==1 || positions_l3[6]==3)
+      && positions_l3[7]==2
+      && positions_l3[8]==2
+      && (positions_l3[9]==0 || positions_l3[9]==2)
+      && positions_l3[10]==0
+      && (positions_l3[11]==1 || positions_l3[11]==3)
+      && positions_l3[12]==1
+      && (positions_l3[13]==0 || positions_l3[13]==2)
+      && (positions_l3[14]==0 || positions_l3[14]==2)
+      && positions_l3[15]==0
+  ) {
+
+    gameobjectsactive()
+    gamewin()
+
+  setTimeout(() => {
+    appear(welldonescreen)
+  }, 2000);
+    
+  }
+
+}
+
+function gamewin(level){
+
+
+  if(level=="level1a" || level=="level1b"){
+    gameobjectsactive()
+  // Set timer for appearance of level 2 button 
+  setTimeout(() => {
+    appear(continuel2)
+
+  }, 4000);
+  }
+
+  if(level=="level2a" || level=="level2b"){
+    gameobjectsactive()
+  // Set timer for appearance of level 2 button 
+  setTimeout(() => {
+    appear(continuel3)
+
+  }, 4000);
+  }
+  
+  appear(video)
+  video.innerHTML = '<video playsinline class="welldonevideo item" autoplay>  <source src="assets/animations/test.mp4" type="video/mp4"></video>'
+
+  playSound(win)
+} 
+
+continuel2.addEventListener("pointerdown", startlevel2)
+continuel3.addEventListener("pointerdown", startlevel3)
+
+function gameobjectsidle(){
+  turbine.classList.add("turbinebefore")
+  turbine.classList.remove("turbineglow")
+  lightbulb.classList.add("lightbulbbefore")
+  lightbulb.classList.remove("lightbulbglow")
+}
+
+function gameobjectsactive(){
+  turbine.classList.remove("turbinebefore")
+  turbine.classList.add("turbineglow")
+  lightbulb.classList.remove("lightbulbbefore")
+  lightbulb.classList.add("lightbulbglow")
+}
+
 
---
+++ rotate.js
@@ -0,0 +1,227 @@
+tilesl1[0].addEventListener("pointerdown", rotatel1_1)
+tilesl1[1].addEventListener("pointerdown", rotatel1_2)
+tilesl1[2].addEventListener("pointerdown", rotatel1_3)
+tilesl1[3].addEventListener("pointerdown", rotatel1_4)
+tilesl1[4].addEventListener("pointerdown", rotatel1_5)
+tilesl1[5].addEventListener("pointerdown", rotatel1_6)
+tilesl1[6].addEventListener("pointerdown", rotatel1_7)
+tilesl1[7].addEventListener("pointerdown", rotatel1_8)
+
+function rotatel1_1() {rotate(0,tilesl1,positions_l1)}
+function rotatel1_2() {rotate(1,tilesl1,positions_l1)}
+function rotatel1_3() {rotate(2,tilesl1,positions_l1)}
+function rotatel1_4() {rotate(3,tilesl1,positions_l1)}
+function rotatel1_5() {rotate(4,tilesl1,positions_l1)}
+function rotatel1_6() {rotate(5,tilesl1,positions_l1)}
+function rotatel1_7() {rotate(6,tilesl1,positions_l1)}
+function rotatel1_8() {rotate(7,tilesl1,positions_l1)}
+
+tilesl2[0].addEventListener("pointerdown", rotatel2_1)
+tilesl2[1].addEventListener("pointerdown", rotatel2_2)
+tilesl2[2].addEventListener("pointerdown", rotatel2_3)
+tilesl2[3].addEventListener("pointerdown", rotatel2_4)
+tilesl2[4].addEventListener("pointerdown", rotatel2_5)
+tilesl2[5].addEventListener("pointerdown", rotatel2_6)
+tilesl2[6].addEventListener("pointerdown", rotatel2_7)
+tilesl2[7].addEventListener("pointerdown", rotatel2_8)
+tilesl2[8].addEventListener("pointerdown", rotatel2_9)
+// tilesl2[9].addEventListener("pointerdown", rotatel2_10)
+// tilesl3[10].addEventListener("pointerdown", rotatel2_11)
+// tilesl2[11].addEventListener("pointerdown", rotatel2_12)
+// tilesl2[12].addEventListener("pointerdown", rotatel2_13)
+// tilesl2[13].addEventListener("pointerdown", rotatel2_14)
+// tilesl2[14].addEventListener("pointerdown", rotatel2_15)
+// tilesl2[15].addEventListener("pointerdown", rotatel2_16)
+// tilesl2[16].addEventListener("pointerdown", rotatel2_17)
+// tilesl2[17].addEventListener("pointerdown", rotatel2_18)
+// tilesl3[18].addEventListener("pointerdown", rotatel2_19)
+// tilesl2[19].addEventListener("pointerdown", rotatel2_20)
+// tilesl2[20].addEventListener("pointerdown", rotatel2_21)
+// tilesl2[21].addEventListener("pointerdown", rotatel2_22)
+// tilesl2[22].addEventListener("pointerdown", rotatel2_23)
+// tilesl2[23].addEventListener("pointerdown", rotatel2_24)
+// tilesl2[24].addEventListener("pointerdown", rotatel2_25)
+// tilesl2[25].addEventListener("pointerdown", rotatel2_26)
+// tilesl3[26].addEventListener("pointerdown", rotatel2_27)
+// tilesl2[27].addEventListener("pointerdown", rotatel2_28)
+// tilesl2[28].addEventListener("pointerdown", rotatel2_29)
+// tilesl2[29].addEventListener("pointerdown", rotatel2_30)
+// tilesl2[30].addEventListener("pointerdown", rotatel2_31)
+// tilesl2[31].addEventListener("pointerdown", rotatel2_32)
+// tilesl3[32].addEventListener("pointerdown", rotatel2_33)
+// tilesl2[33].addEventListener("pointerdown", rotatel2_34)
+// tilesl2[34].addEventListener("pointerdown", rotatel2_35)
+// tilesl2[35].addEventListener("pointerdown", rotatel2_36)
+
+function rotatel2_1() {rotate(0,tilesl2,positions_l2)}
+function rotatel2_2() {rotate(1,tilesl2,positions_l2)}
+function rotatel2_3() {rotate(2,tilesl2,positions_l2)}
+function rotatel2_4() {rotate(3,tilesl2,positions_l2)}
+function rotatel2_5() {rotate(4,tilesl2,positions_l2)}
+function rotatel2_6() {rotate(5,tilesl2,positions_l2)}
+function rotatel2_7() {rotate(6,tilesl2,positions_l2)}
+function rotatel2_8() {rotate(7,tilesl2,positions_l2)}
+function rotatel2_9() {rotate(8,tilesl2,positions_l2)}
+function rotatel2_10() {rotate(9,tilesl2,positions_l2)}
+function rotatel2_11() {rotate(10,tilesl2,positions_l2)}
+function rotatel2_12() {rotate(11,tilesl2,positions_l2)}
+function rotatel2_13() {rotate(12,tilesl2,positions_l2)}
+function rotatel2_14() {rotate(13,tilesl2,positions_l2)}
+function rotatel2_15() {rotate(14,tilesl2,positions_l2)}
+function rotatel2_16() {rotate(15,tilesl2,positions_l2)}
+function rotatel2_17() {rotate(16,tilesl2,positions_l2)}
+function rotatel2_18() {rotate(17,tilesl2,positions_l2)}
+function rotatel2_19() {rotate(18,tilesl2,positions_l2)}
+function rotatel2_20() {rotate(19,tilesl2,positions_l2)}
+function rotatel2_21() {rotate(20,tilesl2,positions_l2)}
+function rotatel2_22() {rotate(21,tilesl2,positions_l2)}
+function rotatel2_23() {rotate(22,tilesl2,positions_l2)}
+function rotatel2_24() {rotate(23,tilesl2,positions_l2)}
+function rotatel2_25() {rotate(24,tilesl2,positions_l2)}
+function rotatel2_26() {rotate(25,tilesl2,positions_l2)}
+function rotatel2_27() {rotate(26,tilesl2,positions_l2)}
+function rotatel2_28() {rotate(27,tilesl2,positions_l2)}
+function rotatel2_29() {rotate(28,tilesl2,positions_l2)}
+function rotatel2_30() {rotate(29,tilesl2,positions_l2)}
+function rotatel2_31() {rotate(30,tilesl2,positions_l2)}
+function rotatel2_32() {rotate(31,tilesl2,positions_l2)}
+function rotatel2_33() {rotate(32,tilesl2,positions_l2)}
+function rotatel2_34() {rotate(33,tilesl2,positions_l2)}
+function rotatel2_35() {rotate(34,tilesl2,positions_l2)}
+function rotatel2_36() {rotate(35,tilesl2,positions_l2)}
+
+
+tilesl3[0].addEventListener("pointerdown", rotatel3_1)
+tilesl3[1].addEventListener("pointerdown", rotatel3_2)
+tilesl3[2].addEventListener("pointerdown", rotatel3_3)
+tilesl3[3].addEventListener("pointerdown", rotatel3_4)
+tilesl3[4].addEventListener("pointerdown", rotatel3_5)
+tilesl3[5].addEventListener("pointerdown", rotatel3_6)
+tilesl3[6].addEventListener("pointerdown", rotatel3_7)
+tilesl3[7].addEventListener("pointerdown", rotatel3_8)
+tilesl3[8].addEventListener("pointerdown", rotatel3_9)
+tilesl3[9].addEventListener("pointerdown", rotatel3_10)
+tilesl3[10].addEventListener("pointerdown", rotatel3_11)
+tilesl3[11].addEventListener("pointerdown", rotatel3_12)
+tilesl3[12].addEventListener("pointerdown", rotatel3_13)
+tilesl3[13].addEventListener("pointerdown", rotatel3_14)
+tilesl3[14].addEventListener("pointerdown", rotatel3_15)
+tilesl3[15].addEventListener("pointerdown", rotatel3_16)
+// tilesl3[16].addEventListener("pointerdown", rotatel3_17)
+// tilesl3[17].addEventListener("pointerdown", rotatel3_18)
+// tilesl3[18].addEventListener("pointerdown", rotatel3_19)
+// tilesl3[19].addEventListener("pointerdown", rotatel3_20)
+// tilesl3[20].addEventListener("pointerdown", rotatel3_21)
+// tilesl3[21].addEventListener("pointerdown", rotatel3_22)
+// tilesl3[22].addEventListener("pointerdown", rotatel3_23)
+// tilesl3[23].addEventListener("pointerdown", rotatel3_24)
+// tilesl3[24].addEventListener("pointerdown", rotatel3_25)
+// tilesl3[25].addEventListener("pointerdown", rotatel3_26)
+// tilesl3[26].addEventListener("pointerdown", rotatel3_27)
+// tilesl3[27].addEventListener("pointerdown", rotatel3_28)
+// tilesl3[28].addEventListener("pointerdown", rotatel3_29)
+// tilesl3[29].addEventListener("pointerdown", rotatel3_30)
+// tilesl3[30].addEventListener("pointerdown", rotatel3_31)
+// tilesl3[31].addEventListener("pointerdown", rotatel3_32)
+// tilesl3[32].addEventListener("pointerdown", rotatel3_33)
+// tilesl3[33].addEventListener("pointerdown", rotatel3_34)
+// tilesl3[34].addEventListener("pointerdown", rotatel3_35)
+// tilesl3[35].addEventListener("pointerdown", rotatel3_36)
+// tilesl3[36].addEventListener("pointerdown", rotatel3_37)
+// tilesl3[37].addEventListener("pointerdown", rotatel3_38)
+// tilesl3[38].addEventListener("pointerdown", rotatel3_39)
+// tilesl3[39].addEventListener("pointerdown", rotatel3_40)
+// tilesl3[40].addEventListener("pointerdown", rotatel3_41)
+// tilesl3[41].addEventListener("pointerdown", rotatel3_42)
+// tilesl3[42].addEventListener("pointerdown", rotatel3_43)
+// tilesl3[43].addEventListener("pointerdown", rotatel3_44)
+// tilesl3[44].addEventListener("pointerdown", rotatel3_45)
+// tilesl3[45].addEventListener("pointerdown", rotatel3_46)
+// tilesl3[46].addEventListener("pointerdown", rotatel3_47)
+// tilesl3[47].addEventListener("pointerdown", rotatel3_48)
+// tilesl3[48].addEventListener("pointerdown", rotatel3_49)
+// tilesl3[49].addEventListener("pointerdown", rotatel3_50)
+// tilesl3[50].addEventListener("pointerdown", rotatel3_51)
+// tilesl3[51].addEventListener("pointerdown", rotatel3_52)
+// tilesl3[52].addEventListener("pointerdown", rotatel3_53)
+// tilesl3[53].addEventListener("pointerdown", rotatel3_54)
+// tilesl3[54].addEventListener("pointerdown", rotatel3_55)
+// tilesl3[55].addEventListener("pointerdown", rotatel3_56)
+// tilesl3[56].addEventListener("pointerdown", rotatel3_57)
+// tilesl3[57].addEventListener("pointerdown", rotatel3_58)
+// tilesl3[58].addEventListener("pointerdown", rotatel3_59)
+// tilesl3[59].addEventListener("pointerdown", rotatel3_60)
+// tilesl3[60].addEventListener("pointerdown", rotatel3_61)
+// tilesl3[61].addEventListener("pointerdown", rotatel3_62)
+// tilesl3[62].addEventListener("pointerdown", rotatel3_63)
+// tilesl3[63].addEventListener("pointerdown", rotatel3_64)
+
+function rotatel3_1() {rotate(0,tilesl3,positions_l3)}
+function rotatel3_2() {rotate(1,tilesl3,positions_l3)}
+function rotatel3_3() {rotate(2,tilesl3,positions_l3)}
+function rotatel3_4() {rotate(3,tilesl3,positions_l3)}
+function rotatel3_5() {rotate(4,tilesl3,positions_l3)}
+function rotatel3_6() {rotate(5,tilesl3,positions_l3)}
+function rotatel3_7() {rotate(6,tilesl3,positions_l3)}
+function rotatel3_8() {rotate(7,tilesl3,positions_l3)}
+function rotatel3_9() {rotate(8,tilesl3,positions_l3)}
+function rotatel3_10() {rotate(9,tilesl3,positions_l3)}
+function rotatel3_11() {rotate(10,tilesl3,positions_l3)}
+function rotatel3_12() {rotate(11,tilesl3,positions_l3)}
+function rotatel3_13() {rotate(12,tilesl3,positions_l3)}
+function rotatel3_14() {rotate(13,tilesl3,positions_l3)}
+function rotatel3_15() {rotate(14,tilesl3,positions_l3)}
+function rotatel3_16() {rotate(15,tilesl3,positions_l3)}
+function rotatel3_17() {rotate(16,tilesl3,positions_l3)}
+function rotatel3_18() {rotate(17,tilesl3,positions_l3)}
+function rotatel3_19() {rotate(18,tilesl3,positions_l3)}
+function rotatel3_20() {rotate(19,tilesl3,positions_l3)}
+function rotatel3_21() {rotate(20,tilesl3,positions_l3)}
+function rotatel3_22() {rotate(21,tilesl3,positions_l3)}
+function rotatel3_23() {rotate(22,tilesl3,positions_l3)}
+function rotatel3_24() {rotate(23,tilesl3,positions_l3)}
+function rotatel3_25() {rotate(24,tilesl3,positions_l3)}
+function rotatel3_26() {rotate(25,tilesl3,positions_l3)}
+function rotatel3_27() {rotate(26,tilesl3,positions_l3)}
+function rotatel3_28() {rotate(27,tilesl3,positions_l3)}
+function rotatel3_29() {rotate(28,tilesl3,positions_l3)}
+function rotatel3_30() {rotate(29,tilesl3,positions_l3)}
+function rotatel3_31() {rotate(30,tilesl3,positions_l3)}
+function rotatel3_32() {rotate(31,tilesl3,positions_l3)}
+function rotatel3_33() {rotate(32,tilesl3,positions_l3)}
+function rotatel3_34() {rotate(33,tilesl3,positions_l3)}
+function rotatel3_35() {rotate(34,tilesl3,positions_l3)}
+function rotatel3_36() {rotate(35,tilesl3,positions_l3)}
+
+
+function rotate(tileno, tilesarrayx, positionsarray){
+    
+    tilesarrayx[tileno].style.pointerEvents="none"
+    tilesarrayx[tileno].classList.remove("rotate90")
+    playSound(swish)
+    rotation= rotationarray[positionsarray[tileno]]+90
+    positionsarray[tileno]++
+    tilesarrayx[tileno].classList.add("rotate90")
+    tilesarrayx[tileno].style.transform = 'rotate('+rotation+'deg)';
+    playSound(swish)
+    setTimeout(() => {
+        tilesarrayx[tileno].style.pointerEvents="auto"
+  
+    if(positionsarray[tileno]==4){
+        positionsarray[tileno] = 0
+        tilesarrayx[tileno].classList.remove("rotate90")
+        tilesarrayx[tileno].style.transform = 'rotate('+0+'deg)';
+    }
+
+
+    // test if the game is complete 
+    if(tilesarrayx==tilesl1) {testgamecompletel1() }
+    else if(tilesarrayx==tilesl2) {testgamecompletel2()}
+    else if (tilesarrayx==tilesl3) {testgamecompletel3() }
+    } , 1000);
+}
+
+
+
+
+
